33eef4ce919f33b28842b7b56971737736e6e1f7
JackHarris <jackjames5@hotmail.com>
2021-02-04 18:46:39 -0800

    Added working list tags endpoint

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -55,7 +55,7 @@ class PublicUserApiTests(TestCase):
         res = self.client.post(CREATE_USER_URL, payload)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
         user_exists = get_user_model().objects.filter(
-            email = payload['email']
+            email=payload['email']
         ).exists()
         self.assertFalse(user_exists)
 
@@ -98,9 +98,9 @@ class PrivateUserApiTests(TestCase):
     """Test API requests that require authentication"""
     def setUp(self):
         self.user = create_user(
-            email = 'test@londonappdev.com',
-            password = 'testpass',
-            name = 'name'
+            email='test@londonappdev.com',
+            password='testpass',
+            name='name'
         )
         self.client = APIClient()
         self.client.force_authenticate(user=self.user)
